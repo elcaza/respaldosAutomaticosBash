b3f50c377386175554a3d0dbd8b22561de08ee58
J.A. Martínez <joseamb95@gmail.com>
2018-03-29 17:01:35 -0600

    Add permisos

--- configRespaldo.sh
+++ configRespaldo.sh
@@ -3,7 +3,7 @@
 # Config para Respaldo automatizado V1
 # @author henkerLove
 
-CARPETA_RESPALDOS="/home/user/respaldos" #contendra los respaldos realizados
+CARPETA_RESPALDOS="/home/`whoami`/respaldos" #contendra los respaldos realizados
 RUTA_SCRIPT="/ubicacion/respaldo.sh" #ubicacion del script
 CARPETA_A_RESPALDAR="/carpeta/" #carpeta que sera respaldada
 COMPRESION="" #| |min|med|max| Tipo de compresion <niguna> <gzip> <bz2> <xz> Comprobar que lo soporta tu S.O
@@ -17,11 +17,12 @@ fi
 
 cd $CARPETA_RESPALDOS
 
-if [ -e $RUTA_SCRIPT ] && [ -x $RUTA_SCRIPT ]; then
+if [ -e $RUTA_SCRIPT ] && [ ! -x $RUTA_SCRIPT ]; then
 	echo "Existe el archivo"
+	chmod +x $RUTA_SCRIPT
 else 
-	echo "No se encuentra el script o no tiene permisos de ejecucion" 
-	echo "No se encuentra el script o no tiene permisos de ejecucion" >> logError.txt
+	echo "No se encuentra el script" 
+	echo "No se encuentra el script" >> logError.txt
 fi
 
 ( exec $RUTA_SCRIPT $CARPETA_A_RESPALDAR $COMPRESION )
